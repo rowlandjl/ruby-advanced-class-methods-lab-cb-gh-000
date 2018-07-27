17ee3b03d70bacc7ca4634cbfd3f2748b8dbfa2c
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-27 12:55:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,7 +29,8 @@ class Song
   end
 
   def self.find_by_name(name)
-    song = self.all.find { |song| song.name == name } 
+    song = self.all.find { |song| song.name == name }
+    song 
   end
 
   def self.find_or_create_by_name
