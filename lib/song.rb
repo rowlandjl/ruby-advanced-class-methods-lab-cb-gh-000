2aab449d020fc2a3c9787237b51ba12d3dd9e051
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-27 12:50:03 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,22 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end 
+
 end
